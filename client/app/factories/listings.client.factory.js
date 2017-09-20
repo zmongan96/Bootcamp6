afa9a9a217aca9109c4d4ed8cd490a3543e64a66
Zoe Mongan <zmongan96@ufl.edu>
2017-09-20 11:57:47 -0400

    fixing client factory

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -1,24 +1,24 @@
-angular.module('listings').factory('Listings', ['$http', 
+angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get(window.location.origin + '/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
-      }, 
+        return $http.post(window.location.origin + 'api/listings', listing);
+      },
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
-      }, 
+        return $http.get(window.location.origin + '/api/listings/' + id);
+      },
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
-      }, 
+        return $http.put(window.location.origin + '/api/listings/' + id, listing);
+      },
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete(window.location.origin + '/api/listings/' + id);
       }
     };
 
